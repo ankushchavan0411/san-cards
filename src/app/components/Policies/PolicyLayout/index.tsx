3acafcf0c9c0068c04433dfabb7312adb8817838
Ankush Chavan <ankushchavan0411@gmail.com>
2025-01-23 14:45:47 +0530

    update

--- src/app/components/Policies/PolicyLayout/index.tsx
+++ src/app/components/Policies/PolicyLayout/index.tsx
@@ -1,6 +1,9 @@
 /** @format */
 
+import Footer from "@/app/components/Footer";
+import Navbar from "@/app/components/Header/Navbar";
 import React from "react";
+import "@/app/globals.css";
 
 type PolicyLayoutProps = {
   title: string;
@@ -9,13 +12,17 @@ type PolicyLayoutProps = {
 
 const PolicyLayout: React.FC<PolicyLayoutProps> = ({ title, children }) => {
   return (
-    <div className="min-h-screen bg-gray-100 text-gray-800">
-      <header className="bg-blue-600 text-white py-4">
-        <h1 className="text-center text-3xl font-bold">{title}</h1>
-      </header>
-      <main className="p-6 max-w-3xl mx-auto bg-white rounded shadow mt-6">
-        {children}
-      </main>
+    <div className="container">
+      <Navbar />
+      <div className="min-h-screen bg-gray-100 text-gray-800">
+        <header className="bg-blue-600 text-white py-4">
+          <h1 className="text-center text-3xl font-bold">{title}</h1>
+        </header>
+        <main className="p-6 max-w-3xl mx-auto bg-white rounded shadow mt-6 mb-6">
+          {children}
+        </main>
+      </div>
+      <Footer />
     </div>
   );
 };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,10 @@
 /** @format */
 
+import Footer from "@/app/components/Footer";
+import Navbar from "@/app/components/Header/Navbar";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import "@/app/globals.css";
 
 export const metadata: Metadata = {
   title: "San Cards - Free Online Marathi Festival & Invitation Cards",
@@ -27,10 +19,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
+        <Footer />
         {children}
+        <Navbar />
       </body>
     </html>
   );
