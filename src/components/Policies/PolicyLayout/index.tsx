e423473d1babdbc5d03de516767a09004420c2db
Ankush Chavan <ankushchavan0411@gmail.com>
2025-01-23 18:41:56 +0530

    fix css

--- src/components/Policies/PolicyLayout/index.tsx
+++ src/components/Policies/PolicyLayout/index.tsx
@@ -11,12 +11,10 @@ type PolicyLayoutProps = {
 const PolicyLayout: React.FC<PolicyLayoutProps> = ({ title, children }) => {
   return (
     <div className="min-h-screen bg-gray-100 text-gray-800">
-      <header className="bg-blue-600 text-white py-4">
+      <header className="bg-orange-400 text-white py-4">
         <h1 className="text-center text-3xl font-bold">{title}</h1>
       </header>
-      <main className="p-6 max-w-3xl mx-auto bg-white rounded shadow mt-6 mb-6">
-        {children}
-      </main>
+      <main className="p-6 mx-auto bg-gray-50 rounded shadow">{children}</main>
     </div>
   );
 };
