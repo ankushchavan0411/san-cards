b8429ae8fdc24cd8709d1197e419f55aab2bca33
Ankush Chavan <ankushchavan0411@gmail.com>
2025-01-23 13:57:47 +0530

    added footer

--- src/app/components/Policies/PolicyNavigationLinks/index.tsx
+++ src/app/components/Policies/PolicyNavigationLinks/index.tsx
@@ -11,16 +11,21 @@ const PolicyNavigationLinks: React.FC = () => {
     },
     { href: "/policies/terms-of-use", label: "Terms of Use" },
     { href: "/policies/copyright-policy", label: "Copyright Policy" },
+    { href: "/contact-us", label: "Contact Us" },
   ];
 
   return (
-    <nav className="mt-2 flex justify-center space-x-4">
+    <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
       {links.map((link, index) => (
-        <Link key={index} href={link.href} className="hover:underline">
+        <Link
+          key={index}
+          href={link.href}
+          className="hover:underline me-4 md:me-6"
+        >
           {link.label}
         </Link>
       ))}
-    </nav>
+    </ul>
   );
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 /** @format */
 
-import PolicyNavigationLinks from "./components/Policies/PolicyNavigationLinks";
+import Footer from "./components/Footer";
+
+// import PolicyNavigationLinks from "./components/Policies/PolicyNavigationLinks";
 
 export default function Home() {
   return (
     <div className="container">
-      <PolicyNavigationLinks />
+      {/* <PolicyNavigationLinks /> */}
+      <Footer />
     </div>
   );
 }
