589c7ea42b6fb26c6e7b01d4bef50ad938e6a96c
Ankush Chavan <ankushchavan0411@gmail.com>
2025-01-23 15:47:35 +0530

    addd logo

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -1,15 +1,16 @@
 /** @format */
 
-import Image from "next/image";
 import PolicyNavigationLinks from "@/app/components/Policies/PolicyNavigationLinks";
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <footer className="bg-white rounded-lg shadow dark:bg-gray-900">
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
-          <a
-            href="https://flowbite.com/"
+          <Link
+            href="/"
             className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
           >
             <Image
@@ -19,7 +20,7 @@ const Footer = () => {
               width={100}
               height={100}
             />
-          </a>
+          </Link>
           <PolicyNavigationLinks />
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
