1c08fe533c6eaad4268d2806d68bef9e56904e8b
Ankush Chavan <ankushchavan0411@gmail.com>
2025-01-23 18:33:15 +0530

    update cta

--- src/components/CTA/index.tsx
+++ src/components/CTA/index.tsx
@@ -7,14 +7,14 @@ const CTA: React.FC = () => {
   return (
     <section className="py-16 bg-orange-400 text-white text-center">
       <h2 className="text-3xl font-bold mb-4">
-        Ready to Create Your Invitation?
+        Ready to Create Your Invitation, Banner?
       </h2>
       <p className="text-lg mb-8">
-        Explore our collection of personalized Marathi festival cards for all
-        occasions.
+        Explore our collection of personalized Marathi festival banners and
+        invitation cards for all occasions.
       </p>
       <Link
-        href="#services"
+        href="/services"
         className="px-8 py-3 bg-white text-blue-600 hover:bg-gray-100 hover:text-blue-700 rounded-full font-medium"
       >
         Explore Our Services
