5117ec05384a2a8e4355be593e8f7ccaa189afdb
Ankush Chavan <ankushchavan0411@gmail.com>
2025-01-25 12:13:43 +0530

    update testimonialsData

--- src/components/Testimonials/index.tsx
+++ src/components/Testimonials/index.tsx
@@ -1,39 +1,20 @@
 /** @format */
 
 import React from "react";
+import { testimonialsData } from "./testimonialsData";
 
 const Testimonials: React.FC = () => {
-  const testimonials = [
-    {
-      name: "Ravi Deshmukh",
-      feedback:
-        "San Utsav made our wedding so special with personalized cards. Their designs are beautiful and unique!",
-      location: "Pune, India",
-    },
-    {
-      name: "Priya Patil",
-      feedback:
-        "I loved the naming ceremony card we ordered from San Utsav. It was exactly what I was looking for.",
-      location: "Mumbai, India",
-    },
-    {
-      name: "Anita Shah",
-      feedback:
-        "The birthday card designs are fantastic! Simple yet elegant. Highly recommended.",
-      location: "Nagpur, India",
-    },
-  ];
-
   return (
     <section className="py-16">
       <div className="text-center mb-12">
-        <h2 className="text-3xl font-bold">What Other Users Say</h2>
+        <h2 className="text-3xl font-bold mb-1">Hear from Our Happy Users</h2>
         <p className="text-lg text-gray-700">
-          Here are a few words from happy users.
+          Discover what our satisfied users have to say about their experience
+          with us.
         </p>
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-        {testimonials.map((testimonial, index) => (
+        {testimonialsData.map((testimonial, index) => (
           <div
             key={index}
             className="bg-gray-50 p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 text-center border border-primary hover:shadow-primary"
---
+++ src/components/Testimonials/testimonialsData.ts
@@ -0,0 +1,46 @@
+import { Testimonial } from "@/types";
+
+export const testimonialsData: Testimonial[] = [
+  {
+    name: "Sneha Patil",
+    feedback:
+      "The designs are amazing, and the platform is so easy to use. My family loved the invites I created for Ganesh Chaturthi!",
+    location: "Mumbai, India",
+  },
+  {
+    name: "Rohan Deshmukh",
+    feedback:
+      "Creating and downloading cards was a seamless experience. Great quality and instant downloads!",
+    location: "Pune, India",
+  },
+  {
+    name: "Aarti Kulkarni",
+    feedback:
+      "I loved the customization options! Adding personal touches to my cards made them extra special.",
+    location: "Nagpur, India",
+  },
+  {
+    name: "Vikram Joshi",
+    feedback:
+      "An excellent eco-friendly solution. The designs are vibrant, and sharing digital invites was so convenient!",
+    location: "Nashik, India",
+  },
+  {
+    name: "Ravi Kendre",
+    feedback:
+      "San Utsav made our wedding so special with personalized cards. Their designs are beautiful and unique!",
+    location: "Sambhajinagar, India",
+  },
+  {
+    name: "Sanmati Sanghai",
+    feedback:
+      "I loved the naming ceremony card we ordered from San Utsav. It was exactly what I was looking for.",
+    location: "Pune, India",
+  },
+  {
+    name: "Anita Shah",
+    feedback:
+      "The birthday card designs are fantastic! Simple yet elegant. Highly recommended.",
+    location: "Nagpur, India",
+  },
+];
