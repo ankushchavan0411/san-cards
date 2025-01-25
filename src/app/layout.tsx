2f884014a69c2d856c2ddd98901765f637066a8b
Ankush Chavan <ankushchavan0411@gmail.com>
2025-01-25 06:56:19 +0530

    fix bg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="container">
+      <body className="container bg-white">
         <Navbar />
         {children}
         <Footer />
