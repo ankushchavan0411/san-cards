3ca1749a083df63df169a4133c6854cf706a6b80
Ankush Chavan <ankushchavan0411@gmail.com>
2025-01-26 13:45:10 +0530

    added DynamicForm

---
+++ src/components/Common/DynamicForm.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import { ErrorMessage, Field, Form, Formik } from "formik";
+
+const DynamicForm = ({
+  fields,
+  initialValues,
+  validationSchema,
+  onSubmit,
+}: {
+  fields: {
+    name: string;
+    label: string;
+    // type: "text" | "textarea" | "file";
+    type: string;
+    placeholder?: string;
+    require?: boolean;
+  }[];
+  initialValues: any;
+  validationSchema: any;
+  onSubmit: (values: any, actions: any) => void;
+}) => {
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      {({ isSubmitting, isValid, setFieldValue, values }) => (
+        <Form className="space-y-4">
+          {fields.map((field) => (
+            <div key={field.name} className="flex flex-col">
+              <label htmlFor={field.name} className="text-sm font-medium">
+                {field.label}
+                {field?.require && (
+                  <span className="text-red-600 font-bold">*</span>
+                )}
+              </label>
+              {field.type === "file" ? (
+                <input
+                  id={field.name}
+                  name={field.name}
+                  type="file"
+                  accept="image/*"
+                  onChange={(e) => {
+                    const file = e.target.files?.[0];
+                    if (file) {
+                      setFieldValue(field.name, file);
+                    }
+                  }}
+                  className="w-full border border-gray-300 focus:border-blue-600 focus:outline-none p-1 px-3"
+                />
+              ) : (
+                <Field
+                  as={field.type === "textarea" ? "textarea" : "input"}
+                  id={field.name}
+                  name={field.name}
+                  placeholder={field.placeholder}
+                  className="w-full border border-gray-300 focus:border-blue-600 focus:outline-none p-1 px-3"
+                />
+              )}
+              <ErrorMessage
+                name={field.name}
+                component="div"
+                className="text-red-600 text-sm mt-1"
+              />
+              {field.type === "file" && values[field.name] && (
+                <p className="text-gray-600 text-sm mt-1">
+                  Selected File: {values[field.name]?.name}
+                </p>
+              )}
+            </div>
+          ))}
+          <button
+            type="submit"
+            disabled={!isValid || isSubmitting}
+            className={`w-full p-2 rounded-lg mt-4 text-white ${
+              isValid
+                ? "bg-primary hover:bg-primary-600 cursor-pointer"
+                : "bg-gray-400 cursor-not-allowed"
+            }`}
+          >
+            Submit
+          </button>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+export default DynamicForm;
--- src/components/TemplateDesign/Marathi/MarathiWeddingCard.tsx
+++ src/components/TemplateDesign/Marathi/MarathiWeddingCard.tsx
@@ -1,30 +1,111 @@
-import React, { useState } from "react";
+"use client";
+
+import React from "react";
 import html2canvas from "html2canvas";
+import * as Yup from "yup";
+import DynamicForm from "@/components/Common/DynamicForm";
 
+// Main Component
 const MarathiWeddingCard: React.FC = () => {
-  const [isSubmit, setIsSubmit] = useState<boolean>(false);
-
-  const [userInfo, setUserInfo] = useState({
+  const initialValues = {
     groomName: "राजवर्धन",
-    groomDetails:
-      "श्री रामचंद्र शंकरराव निकम यांचे नातू व श्री आनंदराव रामचंद्र निकम रा. सांगली, ता. मिरज, जि. सांगली यांचे ज्येष्ठ चिरंजीव",
+    groomDetails: "",
     brideName: "राजनंदिनी",
-    brideDetails:
-      "श्री. आनंदराव तुकाराम पाटील यांचे नात व श्री सदाशिव आनंदराव निकम रा. सांगली, ता. मिरज, जि. सांगली यांची कन्या",
-    weddingDate: "रविवार, दिनांक २१ नोव्हेंबर २०२१",
-    weddingTime: "दु. ३ वा. २१ मि.",
-    venue: "श्री गणेश मल्टिपर्पज हॉल, मलकापूर सातारा रोड, कराड",
-    inviter: "समस्त निकम परिवार",
-    contact: "9876543210",
-  });
-
-  const handleInputChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-  ) => {
-    const { name, value } = e.target;
-    setUserInfo({ ...userInfo, [name]: value });
+    brideDetails: "",
+    weddingDate: "",
+    weddingTime: "",
+    venue: "",
+    inviter: "",
+    contact: "",
+    image: null,
   };
 
+  const validationSchema = Yup.object().shape({
+    groomName: Yup.string().required("Groom's Name is required"),
+    brideName: Yup.string().required("Bride's Name is required"),
+    contact: Yup.string()
+      .matches(/^\d{10}$/, "Invalid contact number")
+      .required("Contact is required"),
+    image: Yup.mixed()
+      .nullable()
+      .test(
+        "fileSize",
+        "File size should be less than 2MB",
+        (value: any) => !value || (value && value.size <= 2 * 1024 * 1024),
+      ),
+  });
+
+  const fields = [
+    {
+      name: "groomName",
+      label: "Groom's Name",
+      type: "text",
+      placeholder: "Groom's Name",
+      require: true,
+    },
+    {
+      name: "groomDetails",
+      label: "Groom's Details",
+      type: "textarea",
+      placeholder: "Groom's Details",
+    },
+    {
+      name: "brideName",
+      label: "Bride's Name",
+      type: "text",
+      placeholder: "Bride's Name",
+      require: true,
+    },
+    {
+      name: "brideDetails",
+      label: "Bride's Details",
+      type: "textarea",
+      placeholder: "Bride's Details",
+    },
+    {
+      name: "weddingDate",
+      label: "Wedding Date",
+      type: "text",
+      placeholder: "Wedding Date",
+    },
+    {
+      name: "weddingTime",
+      label: "Wedding Time",
+      type: "text",
+      placeholder: "Wedding Time",
+    },
+    { name: "venue", label: "Venue", type: "textarea", placeholder: "Venue" },
+    {
+      name: "inviter",
+      label: "Inviter",
+      type: "text",
+      placeholder: "Inviter",
+    },
+    {
+      name: "contact",
+      label: "Contact",
+      type: "text",
+      placeholder: "Contact",
+      require: true,
+    },
+    {
+      name: "image",
+      label: "Upload Image",
+      type: "file",
+      require: true,
+    },
+  ];
+
+  const handleSubmit = (values: any) => {
+    console.log("Submitted Values:", values);
+    if (values.image) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        console.log("Image Data URL:", e.target?.result);
+      };
+      reader.readAsDataURL(values.image);
+    }
+  };
   const downloadCard = async () => {
     const cardElement = document.getElementById("wedding-card");
     if (cardElement) {
@@ -36,158 +117,79 @@ const MarathiWeddingCard: React.FC = () => {
     }
   };
 
+  const userInfo = { ...initialValues };
+
   return (
     <section className="overflow-hidden">
-      <div className="py-2 mx-auto">
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 lg:gap-6">
-          {/* Left Section */}
-          <div className="border border-primary p-4">
-            <h2 className="text-sm title-font text-gray-500 tracking-widest mb-4 uppercase">
-              Enter Invitation Details
+      <div className="py-2 mx-auto grid grid-cols-1 lg:grid-cols-2 gap-4 lg:gap-6">
+        {/* Form Section */}
+        <div className="border border-primary p-4">
+          <h2 className="text-sm title-font text-gray-500 tracking-widest mb-4 uppercase">
+            Enter Invitation Details
+          </h2>
+          <DynamicForm
+            fields={fields}
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={handleSubmit}
+          />
+        </div>
+
+        {/* Preview Section */}
+        <div id="wedding-card" className="border border-primary p-4">
+          <div className="text-center">
+            <h1 className="text-sm font-semibold text-red-700 font-[Tiro+Devanagari+Marathi]">
+              || श्री गणेशाय नमः ||
+            </h1>
+            <img
+              src="/ganesh.png"
+              alt="Ganesh"
+              className="w-16 h-16 mx-auto my-4"
+            />
+          </div>
+          <div className="text-center space-y-4">
+            <h2 className="text-3xl font-bold text-purple-800 font-[Laila]">
+              {userInfo.groomName}
             </h2>
-
-            <div className="space-y-4">
-              <input
-                type="text"
-                name="groomName"
-                value={userInfo.groomName}
-                onChange={handleInputChange}
-                placeholder="Groom's Name"
-                className="w-full border rounded-lg p-2"
-              />
-              <textarea
-                name="groomDetails"
-                value={userInfo.groomDetails}
-                onChange={handleInputChange}
-                placeholder="Groom's Details"
-                className="w-full border rounded-lg p-2"
-              ></textarea>
-              <input
-                type="text"
-                name="brideName"
-                value={userInfo.brideName}
-                onChange={handleInputChange}
-                placeholder="Bride's Name"
-                className="w-full border rounded-lg p-2"
-              />
-              <textarea
-                name="brideDetails"
-                value={userInfo.brideDetails}
-                onChange={handleInputChange}
-                placeholder="Bride's Details"
-                className="w-full border rounded-lg p-2"
-              ></textarea>
-              <input
-                type="text"
-                name="weddingDate"
-                value={userInfo.weddingDate}
-                onChange={handleInputChange}
-                placeholder="Wedding Date"
-                className="w-full border rounded-lg p-2"
-              />
-              <input
-                type="text"
-                name="weddingTime"
-                value={userInfo.weddingTime}
-                onChange={handleInputChange}
-                placeholder="Wedding Time"
-                className="w-full border rounded-lg p-2"
-              />
-              <textarea
-                name="venue"
-                value={userInfo.venue}
-                onChange={handleInputChange}
-                placeholder="Venue"
-                className="w-full border rounded-lg p-2"
-              ></textarea>
-              <input
-                type="text"
-                name="inviter"
-                value={userInfo.inviter}
-                onChange={handleInputChange}
-                placeholder="Inviter"
-                className="w-full border rounded-lg p-2"
-              />
-              <input
-                type="text"
-                name="contact"
-                value={userInfo.contact}
-                onChange={handleInputChange}
-                placeholder="Contact"
-                className="w-full border rounded-lg p-2"
-              />
-            </div>
-            <button
-              onClick={() => setIsSubmit(true)}
-              className="w-full bg-primary text-white p-2 rounded-lg mt-4 hover:bg-primary-600 cursor-pointer"
-            >
-              Submit
-            </button>
+            <p className="text-gray-700">{userInfo.groomDetails}</p>
+            <h2 className="text-3xl font-bold text-purple-800 font-[Laila]">
+              {userInfo.brideName}
+            </h2>
+            <p className="text-gray-700">{userInfo.brideDetails}</p>
           </div>
-
-          {/* Right Section */}
-          <div className="border border-primary p-4">
-            <div className="text-center">
-              <h1 className="text-sm font-semibold text-red-700 font-[Tiro+Devanagari+Marathi]">
-                || श्री गणेशाय नमः ||
-              </h1>
-              <img
-                src="/ganesh.png"
-                alt="Ganesh"
-                className="w-16 h-16 mx-auto my-4"
-              />
-            </div>
-
-            <div className="text-center space-y-4">
-              <h2 className="text-3xl font-bold text-purple-800 font-[Laila]">
-                {userInfo.groomName}
-              </h2>
-              <p className="text-gray-700">{userInfo.groomDetails}</p>
-
-              <h2 className="text-3xl font-bold text-purple-800 font-[Laila]">
-                {userInfo.brideName}
-              </h2>
-              <p className="text-gray-700">{userInfo.brideDetails}</p>
-            </div>
-
-            <div className="text-center mt-8">
-              <h2 className="text-4xl font-extrabold text-pink-600 font-[Tiro+Devanagari+Marathi]">
-                शुभविवाह
-              </h2>
-            </div>
-
-            <div className="text-center mt-6 space-y-4">
-              <p className="text-gray-600">{userInfo.weddingDate}</p>
-              <p className="text-gray-600">{userInfo.weddingTime}</p>
-              <p className="text-gray-600">
-                तरी आपण सर्वांनी सहकुटुंब सहपरिवार उपस्थित राहावे ही विनंती.
-              </p>
-            </div>
-
-            <div className="mt-10">
-              <div className="text-center border-t border-gray-300 pt-4 space-y-4">
-                <h3 className="text-lg font-semibold text-gray-700 font-[Tiro+Devanagari+Marathi]">
-                  विवाहस्थळ
-                </h3>
-                <p className="text-gray-600">{userInfo.venue}</p>
-              </div>
-              <div className="text-center border-t border-gray-300 pt-4 space-y-4 mt-4">
-                <h3 className="text-lg font-semibold text-gray-700 font-[Tiro+Devanagari+Marathi]">
-                  निमंत्रक
-                </h3>
-                <p className="text-gray-600">{userInfo.inviter}</p>
-                <p className="text-gray-600">संपर्क: {userInfo.contact}</p>
-              </div>
-            </div>
-
-            <button
-              disabled={!isSubmit}
-              onClick={downloadCard}
-              className={`w-full p-2 rounded-lg mt-4 text-white ${isSubmit ? "cursor-pointer bg-primary  hover:bg-primary-600" : "cursor-not-allowed bg-gray-400"}`}
-            >
-              Download Card
-            </button>
+          <div className="text-center mt-8">
+            <h2 className="text-4xl font-extrabold text-pink-600 font-[Tiro+Devanagari+Marathi]">
+              शुभविवाह
+            </h2>
           </div>
+          <div className="text-center mt-6 space-y-4">
+            <p className="text-gray-600">{userInfo.weddingDate}</p>
+            <p className="text-gray-600">{userInfo.weddingTime}</p>
+            <p className="text-gray-600">
+              तरी आपण सर्वांनी सहकुटुंब सहपरिवार उपस्थित राहावे ही विनंती.
+            </p>
+          </div>
+          <div className="mt-10">
+            <div className="text-center border-t border-gray-300 pt-4 space-y-4">
+              <h3 className="text-lg font-semibold text-gray-700 font-[Tiro+Devanagari+Marathi]">
+                विवाहस्थळ
+              </h3>
+              <p className="text-gray-600">{userInfo.venue}</p>
+            </div>
+            <div className="text-center border-t border-gray-300 pt-4 space-y-4 mt-4">
+              <h3 className="text-lg font-semibold text-gray-700 font-[Tiro+Devanagari+Marathi]">
+                निमंत्रक
+              </h3>
+              <p className="text-gray-600">{userInfo.inviter}</p>
+              <p className="text-gray-600">संपर्क: {userInfo.contact}</p>
+            </div>
+          </div>
+          <button
+            onClick={downloadCard}
+            className="w-full p-2 rounded-lg mt-4 text-white bg-primary hover:bg-primary-600"
+          >
+            Download Card
+          </button>
         </div>
       </div>
     </section>
